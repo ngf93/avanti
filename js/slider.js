77644a5903c242e73d645b4c8b7e87a0a2cf18ee
Guzel <nabiullinaguzel93@gmail.com>
2024-08-30 10:25:13 +0300

    inner

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'vertical',
+    loop: false,
+    pagination: false,
+    navigation: false,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    mousewheel: {
+        enabled: true,
+        releaseOnEdges: true,
+    },
+  });
